2bd39344989c08f21956346627314120eadb83bb
uri121 <robinov121@gmail.com>
2019-11-23 14:30:48 +0200

    Updates

---
+++ precache-manifest.e95ccc577a3a920de04dc5849450ce9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e46b8623b11129defec1d52b86e18a67",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "1b1a82ac9d80bf8e4ff1",
+    "url": "/todo-list/static/css/main.0cc24ed0.chunk.css"
+  },
+  {
+    "revision": "0d892cdf5afb96d828b0",
+    "url": "/todo-list/static/js/2.b5b2145f.chunk.js"
+  },
+  {
+    "revision": "1b1a82ac9d80bf8e4ff1",
+    "url": "/todo-list/static/js/main.b167b92d.chunk.js"
+  },
+  {
+    "revision": "da715ee0b4f01398cebe",
+    "url": "/todo-list/static/js/runtime-main.14b3a6b5.js"
+  }
+]);
